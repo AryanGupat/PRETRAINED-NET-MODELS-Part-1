017f4500c1866f142b9877ce59129b4161df0016
AryanGupat <75789231+AryanGupat@users.noreply.github.com>
2021-04-26 20:57:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+function setup()
+{
+canva=createCanvas(450 , 450)
+video=createCapture(VIDEO)
+video.hide()
+canva.position(500 , 200)
+
+
+
+}
+function draw()
+{
+
+}
